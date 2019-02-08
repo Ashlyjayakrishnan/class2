b7884a8b254633323006ebe57cdc35185d72ede8
Ashly <ashlybaby95@gmail.com>
2019-02-08 18:12:01 -0500

    Initial Commit

---
+++ class2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  class2
+//
+//  Created by Ashly Baby on 2019-02-08.
+//  Copyright © 2019 Ashly Baby. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
