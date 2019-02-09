517d8da764383270231accacdddd862bba6583e3
Ashlyjayakrishnan <ashlybaby95@gmail.com>
2019-02-08 19:36:17 -0500

    Array and slice

--- class2/main.swift
+++ class2/main.swift
@@ -62,4 +62,44 @@ if (a.contains(-1000))
 else{
     print("FALSE")
 }
+/////Array
+var g = [10,20,30,40]
+print(g.count)//it will count numbers
+// to display the numbers
+for i in g
+{
+    print(i)
+}
+print(g[0],g[1],g[2],g[3])//helps to display the number in one line
+//will not work
+//print(a[5])
+//(a[5]) = 100
+//print(a[5])
+var o = [Int]()
+o.append(100)
+o.append(200)
+o.append(300)
+print(o[0],o[1],o[2])
+o += [111,222,333]
+print(o)
+print("Values of c array")
+let c = g + o
+for i in c
+{
+    print(i)
+}
+print(" slice Values of C[1..3]array")
+var t = c[1...3]//here we are slicing the array
+print(t)
 
+for i in c{
+    print(i)
+}
+print(" slice Values of C[1]array")
+print(c[1])//here all the values will be displayed
+var f = c[3...10]
+print(f[3])
+//deceleration using array<type
+var z = Array<Int>()
+z.append(5000)
+print(z[0])
